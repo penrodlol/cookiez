c756ea581a2e038afbaa41a85f6cc26d4bb92975
penrodlol <penrod.christian7984@gmail.com>
2020-12-28 15:26:18 -0500

    Placeholder for global graphql error handling.

--- src/app/graphql/apollo-http.config.ts
+++ src/app/graphql/apollo-http.config.ts
@@ -1,25 +1,27 @@
 import { ApolloLink } from '@apollo/client/core';
 import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/client/link/error';
 import { AuthService } from '@auth0/auth0-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { environment } from 'src/environments/environment';
 
-const authLink = (auth: AuthService) => {
-  return setContext(async () => {
-    const token = await auth.getAccessTokenSilently().toPromise();
-    return !token ? {} : {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-  });
-};
+const authLink = (auth: AuthService) => setContext(async () => {
+  const token = await auth.getAccessTokenSilently().toPromise();
+  return !token ? {} : {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+});
+
+const errorLink = () => onError(({ graphQLErrors, networkError }) => { });
 
 export const httpLink = (
   http: HttpLink,
   auth: AuthService
 ) => ApolloLink.from([
   authLink(auth),
+  errorLink(),
   http.create({
     uri: environment.graphql_uri,
   }),
